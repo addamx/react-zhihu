06215f352da6fcb94c538b32424cb30725ff3d49
addams <addams.x@infinitytechnologies.com.cn>
2018-01-29 18:08:01 +0800

    v

--- src/index.js
+++ src/index.js
@@ -3,6 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import {AppContainer} from 'react-hot-loader';
+import Routes from './router';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function renderApp(component) {
+  <AppContainer>
+    <Provider store={store}>
+      <Component />
+    </Provider>
+  </AppContainer>,
+  document.getElementById('#root')
+}
+
+if (module.hot) {
+  module.hot.accept("./router", () => {
+    renderApp(Routes);
+  });
+} else {
+  renderApp(Routes)
+}
+
 registerServiceWorker();
